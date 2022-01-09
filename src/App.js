56666ad90b2b72378e2cc1eeeed1f37a2158d8f4
unknown <danilosucur93@gmail.com>
2022-01-09 21:31:30 +0100

    dodao navigaciju

--- src/App.js
+++ src/App.js
@@ -1,11 +1,50 @@
 import React from 'react';
-import HomePage from './homepage.component';
+import { Routes, Route } from 'react-router-dom';
 import './App.css';
+
+import HomePage from './pages/homepage/homepage.component';
+
+const HatsPage = () => (
+  <div>
+    <h1>HATS PAGE</h1>
+  </div>
+);
+
+const JacketsPage = () => (
+  <div>
+    <h1>JACKETS PAGE</h1>
+  </div>
+);
+
+const SneakersPage = () => (
+  <div>
+    <h1>SNEAKERS PAGE</h1>
+  </div>
+);
+
+const WomensPage = () => (
+  <div>
+    <h1>WOMENS PAGE</h1>
+  </div>
+);
+
+const MensPage = () => (
+  <div>
+    <h1>MENS PAGE</h1>
+  </div>
+);
 
 function App() {
   return (
-    <div >
-      <HomePage/>
+    <div>
+      <Routes>
+        <Route path='/' element={<HomePage/>} />
+        <Route path='/hats' element={<HatsPage/>} />
+        <Route path='/sneakers' element={<SneakersPage/>} />
+        <Route path='/jackets' element={<JacketsPage/>} />
+        <Route path='/womens' element={<WomensPage/>} />
+        <Route path='/mens' element={<MensPage/>} />
+      </Routes>
     </div>
   );
 }
---
+++ src/components/menu-item/menu-item.component.jsx
@@ -0,0 +1,26 @@
+import React from 'react';
+import { useNavigate, useLocation } from 'react-router-dom';
+import './menu-item.styles.scss';
+
+const MenuItem = ({ title, imageUrl, size, linkUrl}) => {
+  console.log(linkUrl);
+  let navigate = useNavigate();
+  let location = useLocation();
+  return(
+    <div className={`${size} menu-item`}
+       onClick={() => {navigate(`${location.pathname}${linkUrl}`)}}>
+    <div
+      className='background-image'
+      style={{
+        backgroundImage: `url(${imageUrl})`
+      }}
+    />
+    <div className='content'>
+      <h1 className='title'>{title.toUpperCase()}</h1>
+      <span className='subtitle'>SHOP NOW</span>
+    </div>
+  </div>
+  )
+};
+
+export default MenuItem;

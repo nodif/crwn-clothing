2cf95178dd11d8f461b16958f5a472782bf61d1b
unknown <danilosucur93@gmail.com>
2022-01-09 23:14:37 +0100

    dodali slike

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import { Routes, Route } from 'react-router-dom';
 import './App.css';
 
 import HomePage from './pages/homepage/homepage.component';
+import ShopPage from './pages/shop/shop.component';
 
 const HatsPage = () => (
   <div>
@@ -39,7 +40,7 @@ function App() {
     <div>
       <Routes>
         <Route path='/' element={<HomePage/>} />
-        <Route path='/hats' element={<HatsPage/>} />
+        <Route path='/shop' element={<ShopPage/>} />
         <Route path='/sneakers' element={<SneakersPage/>} />
         <Route path='/jackets' element={<JacketsPage/>} />
         <Route path='/womens' element={<WomensPage/>} />

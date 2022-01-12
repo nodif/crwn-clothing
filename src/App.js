dac40bc5eb114dbcbbdc493497c507a2019a1a26
unknown <danilosucur93@gmail.com>
2022-01-12 11:52:57 +0100

    sredjeno logovanje

--- src/App.js
+++ src/App.js
@@ -62,8 +62,6 @@ class App extends React.Component {
               ...snapShot.data()
             }
           });
-        
-          console.log(this.state);
         });
       }    
       this.setState({currentUser: userAuth});    
